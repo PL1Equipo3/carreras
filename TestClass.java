5929ae38a247bfcc6af251f67a0b1a4ceaf3baa4
Rodry37 <rodrisolla@gmail.com>
2016-02-24 11:36:12 +0100

    Comentarios ruta y fecha
    Comentarios sobre la ruta absoluta de la base de datos y sobre la fecha en SQL

--- TestClass.java
+++ TestClass.java
@@ -1,3 +1,6 @@
+//Rerdar cambiar la ruta absoluta de la base de datos.
+//Para introducir una fecha por comando SQL: "AAAA/MM/DD HH:MM:SS.000000"
+
 import java.sql.*; 
 import net.ucanaccess.*;
 
@@ -5,15 +8,16 @@ public class TestClass {
 	public static void main(String[] args){
 		try{
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			//Cambiar ruta absoluta.
 			Connection conn=DriverManager.getConnection("jdbc:ucanaccess://C:/Users/Rodry/Desktop/La Carpeta/Universidad/SINF/Base De Datos/BaseDeDatos/Carreras.accdb");
 			Statement s = conn.createStatement();
-			// create a table
+			// Crea una tabla con dos campos: un id y un nombre
             String tableName = "myTable" + String.valueOf((int)(Math.random() * 1000.0));
             String createTable = "CREATE TABLE " + tableName + 
                                  " (id Integer, name Text(32))";
             s.execute(createTable); 
             
-            // enter value into table
+            // Añade valores a la tabla
             for(int i=0; i<25; i++)
             {
               String addRow = "INSERT INTO " + tableName + " VALUES ( " + 
@@ -22,7 +26,7 @@ public class TestClass {
               s.execute(addRow);
             }
             
-            // Fetch table
+            // Consulta que selecciona todos los valores y los imprime por pantalla
             String selTable = "SELECT * FROM " + tableName;
             s.execute(selTable);
             ResultSet rs = s.getResultSet();
